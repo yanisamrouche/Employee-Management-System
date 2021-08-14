8b2eb59bdc0c6cbeb01a813e573af370b31269f1
yanis <yann.amrouche@gmail.com>
2021-08-14 18:09:26 +0200

    adding CRUD Create , Read , Update , Delete : done

--- project/src/main/java/com/example/project/EmployeeController.java
+++ project/src/main/java/com/example/project/EmployeeController.java
@@ -9,34 +9,32 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Date;
 import java.util.ResourceBundle;
-import java.sql.Connection;
+
 public class EmployeeController implements Initializable {
 
     private Label label;
     /********************* TextFields *****************/
     @FXML
-    private TextField tfId;
+    TextField tfId;
     @FXML
-    private TextField tfNom;
+    TextField tfNom;
     @FXML
-    private TextField tfPrenom;
+    TextField tfPrenom;
     @FXML
-    private TextField tfAdresse;
+    TextField tfAdresse;
     @FXML
-    private TextField tfAN;
+    TextField tfAN;
     @FXML
-    private TextField tfLN;
+    TextField tfLN;
     @FXML
-    private TextField tfAge;
+    TextField tfAge;
     @FXML
-    private TextField tfFonction;
+    TextField tfFonction;
     @FXML
-    private TextField tfSalaire;
+    TextField tfSalaire;
 
     /********************* TableView *****************/
     @FXML
@@ -62,30 +60,67 @@ public class EmployeeController implements Initializable {
     TableColumn<Employee, Double> colSalaire ;
     /********************* Buttons *****************/
     @FXML
-    private Button btnAjt;
+    Button btnAjt;
     @FXML
-    private Button btnModif;
+    Button btnModif;
     @FXML
-    private Button btnSupp;
+    Button btnSupp;
 
     /********************* Methods *****************/
 
     @FXML
     private void handleButtonAction(ActionEvent event){
         System.out.println("You Clicked me!");
-        label.setText("ClickClick");
+        if(event.getSource() == btnAjt){
+            insertEmployee();
+        }
+        else if (event.getSource() == btnModif){
+            updateEmployee();
+        }
+        else if(event.getSource() == btnSupp){
+            deleteEmployee();
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         showEmployees();
     }
-    /***** Database instance *****/
-    EmployeeDatabase employeeDatabase = new EmployeeDatabase();
+
+    public Connection getConnection(){
+        Connection connection;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/empproject","yanis","AMROUCHEyanis1998%");
+            return connection;
+        }catch (Exception e){
+            System.out.println("Error : "+e.getMessage());
+            return null;}
+    }
+    public ObservableList<Employee> getEmployeesList(){
+        ObservableList<Employee> employeelist = FXCollections.observableArrayList();
+        Connection connection = getConnection();
+        String query = "SELECT * FROM Employee";
+        Statement st;
+        ResultSet rs;
+        try{
+            st = connection.createStatement();
+            rs = st.executeQuery(query);
+            Employee employees;
+            while (rs.next()){
+                employees = new Employee(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("adresse"), rs.getDate("an"), rs.getString("ln"),rs.getInt("age"),rs.getString("fonction"), rs.getDouble("salaire"));
+                employeelist.add(employees);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return employeelist;
+    }
 
     /** display employees **/
     public void showEmployees(){
-        ObservableList<Employee> list = employeeDatabase.getEmployeesList();
+        ObservableList<Employee> list = getEmployeesList();
         col_id.setCellValueFactory
                 (new PropertyValueFactory<Employee, Integer>("id"));
         colNom.setCellValueFactory
@@ -106,6 +141,81 @@ public class EmployeeController implements Initializable {
                 (new PropertyValueFactory<Employee, Double>("salaire"));
         tvEmp.setItems(list);
     }
+
+    public void executeQuery(String query){
+        Connection connection = getConnection();
+        Statement statement;
+        try{
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertEmployee(){
+        String query = "INSERT INTO Employee VALUES " +
+                "("+  tfId.getText() +
+                ",'" + tfNom.getText() +
+                "','" + tfPrenom.getText() +
+                "','" + tfAdresse.getText() +
+                "','" +tfAN.getText() +
+                "','" + tfLN.getText() +
+                "'," + tfAge.getText() +
+                ",'" + tfFonction.getText() +
+                "','" + tfSalaire.getText() +"')";
+        executeQuery(query);
+        showEmployees();
+    }
+
+    public void updateEmployee(){
+        String query = "UPDATE Employee SET " +
+                "nom = '"+ tfNom.getText() + "'" +
+                ", prenom = '" + tfPrenom.getText() + "'" +
+                ", adresse = '" + tfAdresse.getText() + "'" +
+                ", an = '" + tfAN.getText() + "'" +
+                ", ln = '" + tfLN.getText() + "'" +
+                ", age = '" + tfAge.getText() + "'" +
+                ", fonction = '" + tfFonction.getText() + "'" +
+                ", salaire = '" + tfSalaire.getText() +"'" +
+                "WHERE id = "+ tfId.getText() + "";
+        executeQuery(query);
+        showEmployees();
+    }
+
+    public void deleteEmployee(){
+        String query = "DELETE FROM Employee" +
+                " WHERE id = "+tfId.getText()+"";
+        executeQuery(query);
+        showEmployees();
+        tfId.setText("");
+        tfNom.setText("");
+        tfPrenom.setText("");
+        tfAdresse.setText("");
+        tfAN.setText("");
+        tfLN.setText("");
+        tfAge.setText("");
+        tfFonction.setText("");
+        tfSalaire.setText("");
+    }
+
+    public void displayFromTableView(){
+        TableView.TableViewSelectionModel selectionModel = tvEmp.getSelectionModel();
+        ObservableList<Employee> selectedItems = selectionModel.getSelectedItems();
+        for(Employee employee : selectedItems){
+            tfId.setText(String.valueOf(employee.getId()));
+            tfNom.setText(employee.getNom());
+            tfPrenom.setText(employee.getPrenom());
+            tfAdresse.setText(employee.getAdresse());
+            tfAN.setText(String.valueOf(employee.getAn()));
+            tfLN.setText(employee.getLn());
+            tfAge.setText(String.valueOf(employee.getAge()));
+            tfFonction.setText(employee.getFonction());
+            tfSalaire.setText(String.valueOf(employee.getSalaire()));
+        }
+    }
+
+
 
 
 
