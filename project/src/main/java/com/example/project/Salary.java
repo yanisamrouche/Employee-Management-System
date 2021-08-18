7fc9121438f4ecc63f14546384fac7ef766f1aa4
yanis <yann.amrouche@gmail.com>
2021-08-18 16:53:15 +0200

    update the salary with pourcentage or amount

--- project/src/main/java/com/example/project/Salary.java
+++ project/src/main/java/com/example/project/Salary.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 import java.sql.Connection;
@@ -34,22 +36,79 @@ public class Salary {
     TextField tfOrganisme;
     @FXML
     TextField tfSalaire;
+    @FXML
+    TextField tfPourcentage;
+    @FXML
+    TextField tfMontant;
     /************************* Button *********************/
     @FXML
     Button btn_rech;
+    @FXML
+    Button btn_calcul;
+    @FXML
+    Button btn_eng;
+    @FXML
+    Button btn_v;
+    @FXML
+    RadioButton rbtn_pourcentage;
+    @FXML
+    RadioButton rbtn_montant;
+    @FXML
+    Label label_salaire;
 
 
     public void handleButtonAction(ActionEvent event){
         if (event.getSource() == btn_rech){
             displayEmployeeData();
         }
+        else if(event.getSource() == btn_calcul){
+            if(tfPourcentage.isVisible() && rbtn_pourcentage.isSelected())
+                editSalaryByPourcentage();
+            else if(tfMontant.isVisible() && rbtn_montant.isSelected())
+                editSalaryByAmount();
+        }
+        else if(event.getSource() == btn_v){
+            clearData();
+        }
+        else if(event.getSource()==btn_eng){
+            updateSalary();
+        }
+    }
+
+    public void handleRadioButtonActionPoucentage(ActionEvent event){
+
+          if(event.getSource() == rbtn_pourcentage){
+
+              tfMontant.setVisible(false);
+              tfPourcentage.setVisible(true);
+              if(rbtn_montant.isSelected()){
+                  rbtn_montant.setSelected(false);
+              }
+          }
+
+    }
+
+    public void handleRadioButtonActionAmount(ActionEvent event){
+
+        if (event.getSource() == rbtn_montant){
+            tfPourcentage.setVisible(false);
+            tfMontant.setVisible(true);
+            if(rbtn_pourcentage.isSelected()){
+                rbtn_pourcentage.setSelected(false);
+            }
+        }
     }
 
     public ObservableList<Employee> findEmployee(){
         ObservableList<Employee> employeelist = FXCollections.observableArrayList();
-
-        String query = "SELECT * FROM Employee WHERE id ="+tfRech.getText()+"";
-
+        String entry = tfRech.getText();
+        String[] fullName = entry.split(" ");
+        String query ="";
+        if(fullName.length >=2) {
+             query = "SELECT * FROM Employee WHERE  (nom = '" + fullName[0] + "' AND prenom='" + fullName[1] + "')";
+        }else if(fullName.length == 1){
+            query = "SELECT * FROM Employee WHERE id = " + tfRech.getText() +"";
+        }
         Connection connection = EmployeeController.getConnection();
         Statement st;
         ResultSet rs;
@@ -58,7 +117,9 @@ public class Salary {
             rs = st.executeQuery(query);
             Employee employees;
             while (rs.next()){
-                employees = new Employee(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("adresse"), rs.getDate("an"), rs.getString("ln"),rs.getInt("age"),rs.getString("fonction"),rs.getString("organisme"),rs.getInt("numSecu"),rs.getInt("numCompte"), rs.getDouble("salaire"));
+                employees = new Employee(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("adresse"),
+                        rs.getDate("an"), rs.getString("ln"),rs.getInt("age"),rs.getString("fonction"),
+                        rs.getString("organisme"),rs.getInt("numSecu"),rs.getInt("numCompte"), rs.getDouble("salaire"));
                 employeelist.add(employees);
             }
         }catch (Exception e){
@@ -80,6 +141,45 @@ public class Salary {
             tfOrganisme.setText(employee.getOrganisme());
             tfSalaire.setText(String.valueOf(employee.getSalaire()));
         }
+    }
+
+    public void editSalaryByPourcentage(){
+        double salary = Double.parseDouble(tfSalaire.getText());
+        int pourcentage = Integer.parseInt(tfPourcentage.getText());
+        double deduction = (salary*pourcentage)/100;
+        double finalSalary = salary - deduction;
+        label_salaire.setText(String.valueOf(finalSalary));
+    }
+    public void editSalaryByAmount(){
+        double salary = Double.parseDouble(tfSalaire.getText());
+        double amount = Double.parseDouble(tfMontant.getText());
+        double finalSalary = salary + amount;
+        label_salaire.setText(String.valueOf(finalSalary));
+    }
+
+
+    public void clearData(){
+        tfNom.setText("");
+        tfPrenom.setText("");
+        tfAdresse.setText("");
+        tfAN.setText("");
+        tfLN.setText("");
+        tfAge.setText("");
+        tfFonction.setText("");
+        tfOrganisme.setText("");
+        tfSalaire.setText("");
+        tfRech.setText("");
+        tfPourcentage.setText("");
+        tfMontant.setText("");
+        label_salaire.setText("0");
+        rbtn_pourcentage.setSelected(false);
+        rbtn_montant.setSelected(false);
+    }
+
+    public void updateSalary(){
+        String query = "UPDATE Employee SET "+
+                "salaire = '"+label_salaire.getText()+"'";
+        EmployeeController.executeQuery(query);
     }
 
 
